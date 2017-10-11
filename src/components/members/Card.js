4d91b5d80784d92a3db190323ced944c7269a094
gabidavila <gabidavila@gmail.com>
2017-10-11 19:37:53 -0400

    lint & css

--- src/components/members/Card.js
+++ src/components/members/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Card, Image, Icon } from 'semantic-ui-react';
-import { parties } from "../../helpers/parties";
+import { parties } from '../../helpers/parties';
 
 class MemberCard extends React.Component {
   render() {
-    const profile_image = this.props.data.twitter_picture_url || "/images/default_user.png";
+    const profileImage = this.props.data.twitter_picture_url || '/images/default_user.png';
     return (
       <Card link className="member" color={parties[this.props.data.party].color}>
-        <Image src={profile_image} href={`/member/${this.props.data.pp_member_id}`}/>
+        <Image src={profileImage} href={`/member/${this.props.data.pp_member_id}`}/>
         <Card.Content href={`/member/${this.props.data.pp_member_id}`}>
           <Card.Header>{this.props.data.full_name}</Card.Header>
           <Card.Meta><Icon name='calendar'/> Next election in {this.props.data.next_election}</Card.Meta>
--- src/helpers/parties.js
+++ src/helpers/parties.js
@@ -1,7 +1,7 @@
 const parties = {
-  "I": { color: "green", name: "Independent" },
-  "R": { color: "red", name: "Republican" },
-  "D": { color: "blue", name: "Democrat" }
+  'I': { color: 'green', name: 'Independent' },
+  'R': { color: 'red', name: 'Republican' },
+  'D': { color: 'blue', name: 'Democrat' }
 };
 
 export { parties };
