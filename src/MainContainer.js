63509ac42cf1f5ab76afa2e028628b3d9486f471
gabidavila <gabidavila@gmail.com>
2017-10-21 16:24:11 -0400

    adding credits and menu

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -11,6 +11,12 @@ const MainContainer = (props) => {
         </Grid.Column>
         <Grid.Column width={14}>
           {props.children}
+          <div style={{textAlign: 'center', fontSize: '10px'}}>
+            Data obtained through <a href="https://projects.propublica.org/api-docs/congress-api/">
+            ProPublica Congress API</a>. This website is subject to their Terms of
+            Use according to their licensing. Information here is not guaranteed
+            to be up-to-date and some inaccuracies may be present.
+          </div>
         </Grid.Column>
         <Grid.Column width={1}>
 
--- src/components/interface/TopMenuBar.js
+++ src/components/interface/TopMenuBar.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Container, Image, Menu } from 'semantic-ui-react';
+import { Container, Icon, Menu } from 'semantic-ui-react';
 
 const TopMenuBar = () => {
   return (
     <Menu color='yellow' stackable inverted borderless className='remove-radius'>
       <Container>
-        <Menu.Item as='a' href='/' header>
-          <Image centered
-            src='/images/logo.png' alt='Congress, who is?'
-          />
+        <Menu.Item as='a' href='/' header style={{fontSize: '18px', textAlign: 'center'}}>
+          <Icon name='star' color='red'/> Congress, <br/>
+          Who is?<Icon name='star' color='blue'/>
         </Menu.Item>
         <Menu.Item as='a' href='/'>Home</Menu.Item>
         <Menu.Item as='a' href='/map'>Map</Menu.Item>
         <Menu.Item as='a' href='/compare'>Compare</Menu.Item>
+        <Menu.Item as='a' href='/about'>About</Menu.Item>
       </Container>
     </Menu>
   );
