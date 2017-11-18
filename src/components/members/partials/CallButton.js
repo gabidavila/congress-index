d66b07f1dc06926c6657e15f48c80b7ea42cf702
gabidavila <gabidavila@gmail.com>
2017-11-18 14:15:15 -0500

    call working on twilio

--- src/components/members/partials/CallButton.js
+++ src/components/members/partials/CallButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Form, Button, Label, Icon } from 'semantic-ui-react';
+import Recaptcha from 'react-recaptcha';
 
 const Twilio = window.Twilio;
 
@@ -7,38 +8,59 @@ class CallButton extends React.Component {
   state = {
     isOnCall: false,
     buttonText: 'Call Member, free!',
+    logText: null,
     token: null,
     twilioSetup: null
   }
 
   getToken = () => {
-    if (this.state.token) {
-      this.setupTwilio();
+    if (this.state.token !== null) {
+      return this.setupTwilio();
     }
     return fetch(process.env.REACT_APP_BASE_TWILIO + '/twilio/token?number=' + this.props.member.phone)
       .then((response) => response.json())
-      .then((tokenJson) => this.setState({token: tokenJson.token}), () => this.setupTwilio());
+      .then((tokenJson) => {
+        this.setState({token: tokenJson.token}, () => this.setupTwilio());
+      });
   };
 
   handleCall = () => {
-    this.getToken();
+    if (this.state.twilioSetup) {
+      Twilio.Device.disconnectAll();
+    }
+
+    this.getToken().then(() => {
+      this.makeCall();
+    });
+  };
+
+  makeCall = () => {
+    const params = {
+      Member: this.props.member['full-name'],
+      Chamber: this.props.member['congress-type'],
+      To: this.props.member['phone']
+    };
+
+    return Twilio.Device.connect(params);
   };
 
   setupTwilio = () => {
     return new Promise((resolve, reject) => {
       const setup = Twilio.Device.setup(this.state.token);
+      this.setState({twilioSetup: setup});
 
       Twilio.Device.ready(function (device) {
         this.setState({
           isOnCall: true,
-          buttonText: 'Starting connection.'
+          logText: 'Trying to connect...'
         }, () => resolve(setup));
       }.bind(this));
 
       Twilio.Device.error(function (error) {
         this.setState({
           isOnCall: true,
-          buttonText: 'An error occurred.'
+          buttonText: 'Call Member, free!',
+          logText: 'An error occurred.'
         }, () => {
           console.log(error.message);
           reject(error.message);
@@ -48,14 +70,16 @@ class CallButton extends React.Component {
       Twilio.Device.connect(function (conn) {
         this.setState({
           isOnCall: true,
-          buttonText: 'Connection established.'
+          buttonText: 'End the call.',
+          logText: 'Connection established.'
         });
       }.bind(this));
 
       Twilio.Device.disconnect(function (conn) {
         this.setState({
           isOnCall: false,
-          buttonText: 'Call ended.'
+          buttonText: 'Call Member, free!',
+          logText: 'Call ended.'
         });
       }.bind(this));
     });
@@ -66,7 +90,7 @@ class CallButton extends React.Component {
       <Form>
         <Button size='large' color='green' onClick={this.handleCall} className='remove-radius' floated='right'>
           <Icon name='call square'/>{this.state.buttonText}</Button>
-        {this.state.isOnCall ? <Label basic color='grey' style={{ float: 'right', fontSize: '18px' }}></Label> : null}
+        {this.state.isOnCall ? <Label basic color='grey' style={{ float: 'right', fontSize: '18px' }}>{this.state.logText}</Label> : null}
       </Form>
     );
   };
